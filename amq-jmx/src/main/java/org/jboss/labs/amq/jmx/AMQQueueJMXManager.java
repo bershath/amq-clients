514126fd8eeec5da92d8dc702c43b18c820ac730
bershath <tw.techlist@gmail.com>
2021-07-06 15:56:41 +1000

    Parameterised the destination

--- amq-jmx/src/main/java/org/jboss/labs/amq/jmx/AMQQueueJMXManager.java
+++ amq-jmx/src/main/java/org/jboss/labs/amq/jmx/AMQQueueJMXManager.java
@@ -22,6 +22,8 @@ import java.util.HashMap;
  */
 public class AMQQueueJMXManager {
     private static final String JMX_URL = "service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi";
+    private static  final String queueName = "exampleQueue";
+
     public static void main(String[] args){
         try {
             JMXServiceURL jmxServiceURL = new JMXServiceURL(JMX_URL);
@@ -30,11 +32,17 @@ public class AMQQueueJMXManager {
             credentialsMap.put(JMXConnector.CREDENTIALS, userCredentials);
             JMXConnector jmxConnector = JMXConnectorFactory.connect(jmxServiceURL, credentialsMap);
             MBeanServerConnection connection = jmxConnector.getMBeanServerConnection();
-            ObjectName objectName  =  ObjectName.getInstance("org.apache.activemq.artemis:broker=\"0.0.0.0\",component=addresses,address=\"exampleQueue\",subcomponent=queues,routing-type=\"anycast\",queue=\"exampleQueue\"");
+            ObjectName objectName  =  ObjectName.getInstance("org.apache.activemq.artemis:broker=\"0.0.0.0\",component=addresses,address=\""+queueName+"\",subcomponent=queues,routing-type=\"anycast\",queue=\""+queueName+"\"");
+
             QueueControl queueControl = MBeanServerInvocationHandler.newProxyInstance(connection,objectName,QueueControl.class,false);
             System.out.println("Number of messages in the destination : " + queueControl.countMessages());
             System.out.println("Invoking \"queueControl.removeAllMessages()\". Number of messages removed: " + queueControl.removeAllMessages());
             System.out.println("Number of messages in the destination : " + queueControl.countMessages());
+            queueControl.getMessageCount();
+            queueControl.getMessagesAdded();
+            queueControl.getMessagesAcknowledged();
+            System.out.println("Message counter : " + queueControl.listMessageCounter());
+
             jmxConnector.close();
         } catch (Exception e) {
             e.printStackTrace();
