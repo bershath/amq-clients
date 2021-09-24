700bfa45b5915a48249700d82e1d2b82f0ef26f9
bershath <tw.techlist@gmail.com>
2021-09-24 11:51:49 +1000

    Modified to comply with the API change

--- amq-jmx/src/main/java/org/jboss/labs/amq/jmx/AMQNewQueueCreator.java
+++ amq-jmx/src/main/java/org/jboss/labs/amq/jmx/AMQNewQueueCreator.java
@@ -1,8 +1,6 @@
 package org.jboss.labs.amq.jmx;
 
-import org.apache.activemq.artemis.api.core.QueueConfiguration;
 import org.apache.activemq.artemis.api.core.management.ActiveMQServerControl;
-
 import javax.management.MBeanServerConnection;
 import javax.management.MBeanServerInvocationHandler;
 import javax.management.ObjectName;
@@ -11,8 +9,24 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.util.HashMap;
 
+/**
+ * @author  : Tyronne
+ * @since   : 11-12-2020
+ * @version : 1.2
+ *
+ * Please enable jmx-management-enabled property in the broker.xml file:
+ * <jmx-management-enabled>true</jmx-management-enabled>
+ *
+ * Please configure the connector-host and connector-port in the management.xml file:
+ * <connector connector-host="$JMS_BROKER_HOST" connector-port="1099"/>
+ */
+
 public class AMQNewQueueCreator {
     private static final String JMX_URL = "service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi";
+    private static final String addressName = "testQueue";
+    private static final String queueName = "{\"name\":"+"\""+addressName+"\"" +",\"routing-type\":\"ANYCAST\"}";
+
+
     public static void main(String[] args){
         try {
             JMXServiceURL jmxServiceURL = new JMXServiceURL(JMX_URL);
@@ -23,10 +37,10 @@ public class AMQNewQueueCreator {
             MBeanServerConnection connection = jmxConnector.getMBeanServerConnection();
             ObjectName objectName  =  ObjectName.getInstance("org.apache.activemq.artemis:broker=\"0.0.0.0\"");
             ActiveMQServerControl activeMQServerControl = MBeanServerInvocationHandler.newProxyInstance(connection,objectName,ActiveMQServerControl.class,false);
-            
-            //String queue = activeMQServerControl.createQueue(String.valueOf(queueConfiguration), true);// createQueue(String queueConfiguration, boolean ignoreIfExists)
-//activeMQServerControl.addSecuritySettings("FirstAddress","amq","","amq","","amq","","");
-            //System.out.println("Created  address " + activeMQServerControl.getAddressInfo("FirstAddress"));
+            activeMQServerControl.createQueue(queueName, true);
+            //activeMQServerControl.addSecuritySettings(addressName,"amq","","amq","","amq","","");
+            //addSecuritySettings(String addressMatch, String sendRoles, String consumeRoles, String createDurableQueueRoles, String deleteDurableQueueRoles, String createNonDurableQueueRoles, String deleteNonDurableQueueRoles, String manageRoles, String browseRoles, String createAddressRoles, String deleteAddressRoles)
+            activeMQServerControl.addSecuritySettings(addressName, "amq", "amq","amq","amq","amq", "amq", "amq","amq", "amq","amq");
             jmxConnector.close();
         } catch (Exception e) {
             e.printStackTrace();
