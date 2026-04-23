0c7b7eb29e39ec75e1b02ab31c9b805fa76da25a
bershath <bershath@gmail.com>
2026-04-23 14:02:32 +1000

    Updated

--- amq-jakarta/src/main/java/org/jboss/labs/amq/QueueSender.java
+++ amq-jakarta/src/main/java/org/jboss/labs/amq/QueueSender.java
@@ -13,7 +13,6 @@ import javax.naming.NamingException;
 
 public class QueueSender {
 
-    public static final int numOfMessages = 1;
     public static final String connectionFactoryStr = "ConnectionFactory";
     private static final String userName = "admin";
     private static final String password = "jboss100";
@@ -26,11 +25,9 @@ public class QueueSender {
         try(JMSContext jmsContext = connectionFactory.createContext(userName, password)){
             Queue queue = (Queue) initialContext.lookup(queueJNDIName);
             JMSProducer jmsProducer = jmsContext.createProducer();
-            for (int i = 1; i <= numOfMessages; i++) {
-                TextMessage textMessage = jmsContext.createTextMessage();
-                textMessage.setText("Message from Queue Sender: " + i);
-                jmsProducer.send(queue, textMessage);
-            }
+            TextMessage textMessage = jmsContext.createTextMessage();
+            textMessage.setText("Message from Queue Sender");
+            jmsProducer.send(queue, textMessage);
             JMSConsumer jmsConsumer = jmsContext.createConsumer(queue);
             TextMessage receivedMessage = (TextMessage) jmsConsumer.receive(30000);
             System.out.println(receivedMessage.getBody(String.class));
